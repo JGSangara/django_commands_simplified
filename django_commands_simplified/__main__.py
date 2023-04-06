33d1c36fa4bc32e8373167303076ad70fa34267f
Josphat Gitogo Sangara <josphat.gitogo@gmail.com>
2023-04-06 03:09:19 +0300

    Updated commands

--- django_commands_simplified/__main__.py
+++ django_commands_simplified/__main__.py
@@ -1,14 +1,28 @@
 import argparse
-from .commands import runserver, migrate
+from .commands import startproject, startapp, runserver, makemigrations, migrate, createsuperuser, collectstatic, shell, test
 
-parser = argparse.ArgumentParser(description='My Command-Line Tool')
+parser = argparse.ArgumentParser(description='Django Commands Simplified')
 
 parser.add_argument('command', choices=[
-                    'runserver', 'migrate'], help='The command to run')
+                    'startproject', 'startapp', 'runserver', 'makemigrations', 'migrate', 'createsuperuser', 'collectstatic', 'shell', 'test'], help='The command to run')
 
 args = parser.parse_args()
 
-if args.command == 'runserver':
+if args.command == 'startproject':
+    startproject()
+elif args.command == 'startapp':
+    startapp()
+elif args.command == 'runserver':
     runserver()
+elif args.command == 'makemigrations':
+    makemigrations()
 elif args.command == 'migrate':
     migrate()
+elif args.command == 'createsuperuser':
+    createsuperuser()
+elif args.command == 'collectstatic':
+    collectstatic()
+elif args.command == 'shell':
+    shell()
+elif args.command == 'test':
+    test()
